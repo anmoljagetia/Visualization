9196b927be832f904b2aac6e8915c818062d86a9
Gordon Smith <gordon.smith@lexisnexis.com>
2015-05-22 08:45:41 +0100

    General Refactor
    Signed-off-by: Gordon Smith <gordon.smith@lexisnexis.com>

--- src/other/Slider.js
+++ src/other/Slider.js
@@ -11,50 +11,43 @@
         ISlider.call(this);
 
         this.selectionLabel("");
+        this._playing = false;
+        this._loop = false;
 
         this.xScale = d3.scale.linear()
             .clamp(true)
         ;
-        
+
         var context = this;
-        
-        this._icon = new Icon()
+        this._playIcon = new Icon()
             .faChar("\uf04b")
-            .scale(1.5)
         ;
-
-        this._icon1 = new Icon()
-            .faChar("\uf01e")
-            .scale(1)
-        ;
-
-        this._icon.click = function(d) {
+        this._playIcon.click = function (d) {
             d3.event.stopPropagation();
             if (context._playing) {
-                context._playing = false;
-                context.stop();
-                d
-                    .faChar("\uf04b")
-                    .render()
-                ;
+                context.pause();
             } else {
-                context._playing = true;
                 context.play();
-                d
-                    .faChar("\uf04c")
-                    .render()
-                ;
             }
         };
 
-        this._icon1.click = function(d) {
-            if (context._loop === false) {
-                context._loop = true;
-                console.log ("Loop enabled");
-            } else {
+        this._loopIcon = new Icon()
+            .faChar("\uf01e")
+            .image_colorFill(this._loop ? null : "#bbb")
+            .shape_colorFill(this._loop ? null : "white")
+            .paddingPercent(33)
+        ;
+        this._loopIcon.click = function (d) {
+            if (context._loop) {
                 context._loop = false;
-                console.log ("Loop disabled");
+            } else {
+                context._loop = true;
             }
+            context._loopIcon
+                .image_colorFill(context._loop ? null : "#bbb")
+                .shape_colorFill(context._loop ? null : "white")
+                .render()
+            ;
         };
 
         this.brush = d3.svg.brush()
@@ -71,6 +64,7 @@
         this.axis = d3.svg.axis()
               .scale(this.xScale)
               .orient("bottom")
+              .tickValues(null)
               .tickFormat(function (d) { return d; })
               .tickSize(0)
               .tickPadding(12)
@@ -80,72 +74,80 @@
     Slider.prototype._class += " other_Slider";
     Slider.prototype.implements(ISlider.prototype);
 
-    Slider.prototype.publish("fontSize", null, "number", "Font Size",null,{tags:['Basic']});
-    Slider.prototype.publish("fontFamily", null, "string", "Font Name",null,{tags:['Basic']});
-    Slider.prototype.publish("fontColor", null, "html-color", "Font Color",null,{tags:['Basic']});
+    Slider.prototype.publish("padding", 16, "number", "Outer Padding", null, { tags: ["Basic"] });
+    Slider.prototype.publish("fontSize", null, "number", "Font Size", null, { tags: ["Basic"] });
+    Slider.prototype.publish("fontFamily", null, "string", "Font Name", null, { tags: ["Basic"] });
+    Slider.prototype.publish("fontColor", null, "html-color", "Font Color", null, { tags: ["Basic"] });
 
-    Slider.prototype.publish("showPlay", false, "boolean", "Show Play Button");
-    Slider.prototype.publish("allowRange", false, "boolean", "Allow Range Selection",null,{tags:['Intermediate']});
-    Slider.prototype.publish("low", 0, "number", "Low",null,{tags:['Intermediate']});
-    Slider.prototype.publish("high", 100, "number", "High",null,{tags:['Intermediate']});
-    Slider.prototype.publish("step", 10, "number", "Step",null,{tags:['Intermediate']});
+    Slider.prototype.publish("allowRange", false, "boolean", "Allow Range Selection", null, { tags: ["Intermediate"] });
+    Slider.prototype.publish("low", 0, "number", "Low", null, { tags: ["Intermediate"] });
+    Slider.prototype.publish("high", 100, "number", "High", null, { tags: ["Intermediate"] });
+    Slider.prototype.publish("step", 10, "number", "Step", null, { tags: ["Intermediate"] });
+    Slider.prototype.publish("selectionLabel", "", "string", "Selection Label", null, { tags: ["Intermediate"] });
+
+    Slider.prototype.publish("showPlay", true, "boolean", "Show Play Button");
     Slider.prototype.publish("playInterval", 1000, "number", "Play Interval");
-    Slider.prototype.publish("selectionLabel", "", "string", "Selection Label",null,{tags:['Intermediate']});
-    Slider.prototype.publishProxy("playDiameter", "_icon", "diameter");
-    Slider.prototype.publish("playGutter", 40, "number", "playGutter");
+    Slider.prototype.publishProxy("playDiameter", "_playIcon", "diameter", 32);
+    Slider.prototype.publish("playGutter", 12, "number", "Play Gutter");
+    Slider.prototype.publishProxy("loopDiameter", "_loopIcon", "diameter", 24);
+    Slider.prototype.publish("loopGutter", 4, "number", "Play Gutter");
 
     Slider.prototype.testData = function (_) {
         this.columns("Percent");
-        this.data(66);
+        this.data(20);
         return this;
     };
 
     Slider.prototype.testData2 = function (_) {
         this.allowRange(true);
         this.columns("Percent");
-        this.data([44, 66]);
+        this.data([20, 40]);
         return this;
     };
 
     Slider.prototype.play = function () {
-        var context = this;
-        var tick = this.low(); 
-
-        this.intervalHandler = setInterval(function () { 
-            context 
-                .data(tick) 
-                .render() 
+        this._playing = true;
+        this._playIcon
+            .faChar("\uf04c")
+            .render()
+        ;
+        var tick = this.data();
+        if (tick < this.low() || tick >= this.high()) {
+            tick = this.low();
+            this
+                .data(tick)
+                .render()
             ;
+        }
+        var context = this;
+        this.intervalHandler = setInterval(function () {
             tick += context.step();
             if (tick > context.high()) {
                 if (context._loop === true) {
-                    context.data(context.low());
                     tick = context.low();
-                } else {
-                    context._icon
-                        .faChar("\uf04b")
+                    context
+                        .data(tick)
                         .render()
                     ;
-                    context._playing = false;
-                    
-                    clearInterval(context.intervalHandler);
+                } else {
+                    context.pause();
                 }
+            } else {
+                context
+                    .data(tick)
+                    .render()
+                ;
             }
         }, context.playInterval());
     };
 
     Slider.prototype.pause = function () {
-        // Will be added later
-    };
-
-    Slider.prototype.stop = function () {
-        console.log("stopped");
-        this._icon
+        this._playing = false;
+        this._playIcon
             .faChar("\uf04b")
             .render()
         ;
         clearInterval(this.intervalHandler);
-        this.data(this.low());
     };
 
     Slider.prototype.data = function (_) {
@@ -161,13 +163,6 @@
     };
 
     Slider.prototype.enter = function (domNode, element) {
-        if ((this.high() - this.low()) / this.step() <= 10) {
-            this.axis.tickValues(d3.merge([d3.range(this.low(), this.high(), this.step()), [this.high()]]));
-        }
-
-        this._playing = false;
-        this._loop = false;
-
         this.axisElement = element.append("g")
             .attr("class", "x axis")
         ;
@@ -199,70 +194,53 @@
             .attr("transform", "translate(0,-27)")
         ;
 
-        this._icon
+        this._playIcon
             .target(domNode)
-            .pos({x: this.width()/2 - 80, y: 0})
-            .display(false)
             .render()
-        ;        
+        ;
 
-        this._icon1
+        this._loopIcon
             .target(domNode)
-            .pos({x: this.width()/2 - 40, y: 0})
-            .display(false)
             .render()
         ;
     };
 
     Slider.prototype.update = function (domNode, element) {
         var context = this;
-        var width = this.width() - 100;  //TODO - 50 should be "padding"
-        // var height = this.height() - 20;  //TODO - 20 should be "padding"
+        var width = this.width() - this.padding() * 2;
+
+        this._playIcon
+            .pos({ x: width / 2 - (this.loopDiameter() + this.loopGutter() + this.playDiameter() / 2), y: 0 })
+            .diameter(this.playDiameter())
+            .display(this.showPlay())
+            .render()
+        ;
+
+        this._loopIcon
+            .pos({ x: width / 2 - (this.loopDiameter() / 2), y: 0 })
+            .diameter(this.loopDiameter())
+            .display(this.showPlay())
+            .render()
+        ;
 
         this.xScale
             .domain([this.low(), this.high()])
-            .range([-width/2, width/2])
+            .range([-width / 2, width / 2 - (this.showPlay() ? this.loopDiameter() + this.loopGutter() + this.playDiameter() + this.playGutter() : 0)])
         ;
-         this.data(this._data);
-       
-        if (this.showPlay()) {
-            this._icon
-                .display(true)
-                .render()
-            ;
 
-            this._icon1
-                .display(true)
-                .render()
-            ;
-
-            this.xScale
-                .domain([this.low(), this.high()])
-                .range([-width/2, width/2 - this._icon.diameter()*2 - this.playGutter()])
-            ;
-            this.data(this._data);
-
+        if ((this.high() - this.low()) / this.step() <= 10) {
+            this.axis.tickValues(d3.merge([d3.range(this.low(), this.high(), this.step()), [this.high()]]));
         } else {
-            this._icon
-                .display(false)
-                .render()
-            ;
-
-            this._icon1
-                .display(false)
-                .render()
-            ;
+            this.axis.tickValues(null);
         }
-
-
         this.axisElement
             .call(this.axis)
         ;
 
         this.axisElement.selectAll('.tick > text')
-                .style('fill',this.fontColor())
-                .style('font-size',this.fontSize())
-                .style('font-family',this.fontFamily())
+            .style('fill', this.fontColor())
+            .style('font-size', this.fontSize())
+            .style('font-family', this.fontFamily())
         ;
 
         var range = this.xScale.range();
@@ -275,18 +253,9 @@
             .attr("d", function (d) { return context.handlePath(d); })
         ;
 
-        if (this._initHandle === undefined) {
-            this._initHandle = true;
-            var selVal = [this.low(), this.low()];
-            if (this.allowRange() && this._data) {
-                selVal = this._data;
-            } else if (this._data){
-                selVal = [this._data, this._data];
-            }
-            this.brushg
-                .call(this.brush.extent(selVal))
-            ;
-        }
+        this.brushg
+            .call(this.brush.extent(this.allowRange() ? this._data : [this._data, this._data]))
+        ;
     };
 
     Slider.prototype.brushstart = function (d, self) {
@@ -342,7 +311,7 @@
         var x = e ? 1 : -1;
         var xOffset = this.allowRange() ? 0.5 : 0.0;
         var y = 18;
-        var retVal = "M" + (xOffset * x) + "," + y + 
+        var retVal = "M" + (xOffset * x) + "," + y +
             "A6,6 0 0 " + e + " " + (6.5 * x) + "," + (y + 6) +
             "V" + (2 * y - 6) +
             "A6,6 0 0 " + e + " " + (xOffset * x) + "," + (2 * y)
