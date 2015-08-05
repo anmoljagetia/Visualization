b7b36221ff450e487e2571459d7b5b92fc7496a5
Anmol Jagetia <anmoljagetia@hotmail.com>
2015-08-05 16:31:32 +0530

    Add Unit Tests
    Signed-off-by: Gordon Smith <gordon.smith@lexisnexis.com>

---
+++ test/widgets.js
@@ -0,0 +1,24 @@
+﻿"use strict";
+define(["require"], function (require) {
+    describe("widgets", function () {
+        allWidgets.forEach(function (widget) {
+            var path = widget.path;
+            describe(path, function () {
+                it("require load", function (done) {
+                    require([path], function (Widget) {
+                        done();
+                    });
+                });
+                var pathParts = path.split("/");
+                var className = pathParts[1] + "_" + pathParts[2];
+                it("features", function (done) {
+                    require([path], function (Widget) {
+                        assert.equal(typeof Widget, "function");
+                        assert.isFunction(Widget.prototype.constructor, "constructor");
+                        done();
+                    });
+                });
+            });
+        });
+    });
+});
